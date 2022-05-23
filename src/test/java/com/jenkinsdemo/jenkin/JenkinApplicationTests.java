47eb4bc1c56f12ee0379afc763e525d5a77969f5
吴明度 <15221451167@163.com>
2022-05-23 14:00:52 +0800

    1

---
+++ src/test/java/com/jenkinsdemo/jenkin/JenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsdemo.jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
